c77473374b6a397446d9096f8f1a67be755bbb1a
Sanzhar Beishekeev <franki-55@mail.ru>
2024-09-16 17:45:06 +0600

    В app.tsx вызываю все эти классы, создается два состояния которые заполняются картами. в первое состояние попадает вызов класса и с помощью него заполняем hand массивом из 5 карт с помощью setHand. Создал кнопку для раздачи карты. С помошью перебора map беру из массива hand 5 карт и прорисовываю каждую карту на страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react';
+import './App.css';
+import CardDeck from './components/lib/CardDeck';
+import CardSuits from './components/deckRender/deckRender.tsx';
+import Card from './components/lib/Card';
+import PokerHand from './components/lib/Poker.ts';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [cardDeck] = useState<CardDeck>(new CardDeck());
+  const [hand, setHand] = useState<Card[]>([]);
+  const pokerHand = new PokerHand(hand);
+  const outcome = pokerHand.getOutcome();
+
+
+  const getCardsOnWindow = () => {
+    setHand(cardDeck.getCards(5));
+  };
 
   return (
-    <>
-
-    </>
-  )
+      <>
+        <button onClick={getCardsOnWindow}>Раздать карты</button>
+        {hand.length > 0 ? (
+            <div className='cardWrapper'>
+              <div className="playingCards faceImages">
+                {hand.map((card, index) => (
+                    <CardSuits key={index} rank={`${card.rank}`} suit={`${card.suit}`}/>
+                ))}
+                <p>{outcome}</p>
+              </div>
+            </div>
+        ) : <p>Раздайте карты</p>
+        }
+      </>
+  );
 }
 
-export default App
+export default App;
